04cec17c313aa6fa7fad281f321849d2ec325348
HirayamaSachi <hirayamas0305@gmail.com>
2023-01-08 14:01:28 +0900

    add:天気情報のjson取得

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'location.dart';
+import 'package:http/http.dart' as http;
+import '../apiKey.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,17 +11,27 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   LocationPermission permission;
-  void getCurrentPosition() async{
+  Future<Location> getCurrentPosition() async {
     Location location = Location();
     await location.getCurrentPosition();
-    print(location.latitude);
-    print(location.longitude);
+    return location;
+  }
+
+  void getData() async {
+    Location location = await getCurrentPosition();
+    var response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}'));
+    if (response.statusCode==200) {
+        print(response.body);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getCurrentPosition();
+    getData();
   }
 
   @override
