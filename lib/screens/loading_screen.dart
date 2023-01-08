55047ae94082fa94fb8242732d1c40cf9852ca41
HirayamaSachi <hirayamas0305@gmail.com>
2023-01-08 14:25:56 +0900

    add:jsonDecode

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'location.dart';
 import 'package:http/http.dart' as http;
 import '../apiKey.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,14 +18,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentPosition();
     return location;
-  }
+    }
 
   void getData() async {
     Location location = await getCurrentPosition();
     var response = await http.get(Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}'));
     if (response.statusCode==200) {
-        print(response.body);
+      var data=jsonDecode(response.body);
+      double temp=data['main']['temp'];
+      int conditionNumber=data['weather'][0]['id'];
+      String cityName=data['name'];
+      print(temp);
+      print(conditionNumber);
+      print(cityName);
+      
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
